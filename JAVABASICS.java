ef64bdeb03bb198efa4b4793a3ab1f702e83d093
Sushant-arch-star <Sushantji045@gmail.com>
2026-08-08 14:39:11 +0530

    Added JAVABASICS pracctise

---
+++ JAVABASICS.java
@@ -0,0 +1,201 @@
+/////////// DATA TYPES THROUGH LIVE SESSIONS //////////////////////////
+// /// 
+// public class Main {
+//    public static void main(String[] args) {
+//       // int marks = 65;
+        
+// // //         System.out.println("marks");
+// // //         System.out.println(marks);
+//                 System.out.println('1' + '2'); 
+// //         char symbol =(char)marks;}
+// //         System.out.println(symbol);
+        
+        // char s ='q';
+        // int r = s;
+        // System.out.println(r);
+        // // or can be written as a
+           //     System.out.println((int)'q');
+//                       System.out.println(15 / 2);
+//          System.out.println(15 / 2.0);
+
+    // range of intergers -->   -2b to 2b
+        
+         
+       // System.out.println(Integer.MAX_VALUE);
+               //  System.out.println(Integer.MAX_VALUE + 1);
+// KYU KI YEAH MAX VALUE H INT KI 4 BYTES FULL TO 1+ KARE TO KYA INT SA VALUE HO JAYEGI NO 
+      //  ANS IS - INFINITY OPTION AND VISE VERSA FOR SAME Integer.MAX_VALUE + 1);
+      //  ==== -2147483648 WHICH IS MIN_VALUE SO VICE VERSA OF OTHER OKAY
+        
+        
+   // }
+ //}
+///////////////////////////////////////////////////////////////////////////
+// public class JAVABASICS {
+//     public static void main(String arg[]) {
+//         System.out.println("sushant tiwari");
+//         System.out.println("sushant tiwari");
+
+//         System.out.println("sushant tiwari");
+
+
+
+        
+    // }
+
+// }
+// boilerplate code
+
+// public class JAVABASICS {
+    //  public static void main(String args[]) {
+        // System.out.println("****");
+        // System.out.println("***");
+        // System.out.println("**");
+        // System.out.println("*");
+
+    //      int a = 10;
+    //    int b = 20;
+    //     System.out.println(a);
+    //    String name = "sush";
+    //    System.out.println(name);
+    //    a = b;
+    //    System.out.println(a);
+
+
+/// data types 
+
+    //    byte b = 6;
+    //    System.out.println(b);
+    //    char ch = 'a';
+    //    System.out.println(ch);
+    //    boolean var = true;
+    //    float price = 10.5;
+    //    int  number = 25;
+    //    //long 
+    //    // double 
+       
+    //     short n = 240;
+
+
+    // }
+
+
+// }
+
+ //////////// inputs in java ////////
+ /// 
+//  ///
+//  import java.util.*;
+//  public class JAVABASICS {
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//                 String name  = sc.next();
+//   System.out.println(name); 
+// // /
+// //         String name = sc.nextLine();
+//         System.out.println(name);
+
+//         int number = sc.nextInt();
+//         System.out.println(number);
+
+//         float price = sc.nextFloat();
+//         System.out.println(price);
+//             Scanner sc = new Scanner(System.in);
+
+//             float rad = sc.nextFloat();
+//             float area = 3.14f * rad * rad;
+
+
+//             System.out.println(area);
+
+//     }
+// }
+
+// import java.util.*;
+// public class JAVABASICS {
+//     public static void main(String args[]) {
+//      Scanner sc = new Scanner(System.in);
+//      double r  = sc.nextDouble();
+//           double s  = sc.nextDouble();
+//           double product = r*s;
+//           System.out.println(product);
+
+// }
+
+// }
+
+// ///////////////////////////////
+// //////////////////////////////////////////
+   // TYPE CONVERSION ///////////
+// // // import java.util.*;
+// // public class JAVABASICS {
+// //     public static void main(String args[]) {
+// //         //  int a = 15;
+// //         //  long b = a;
+// WE CAN store int a in ling b  but vice versa cant be possible becayse cant store big value in small size storage 
+// //         // Scanner sc = new Scanner(System.in);
+// //         // float number = sc.nextInt();
+       // can be done float to int  asign done but vice versa cant be done 
+
+       ///////////// type casting /////////////
+// //         // float a = 25.1f;
+// //         // int b =  (int) a;
+//  // # TYPE PROMOTION 
+
+// //         // char a = 'a';
+// //         // char b =  'b';
+// //         // char c = a-b; // cant be done 
+
+// //         //  System.out.println((int)(b));
+// //         //  System.out.println((int)(a));
+//         //  System.out.println(b-a);
+
+// //         // short q = 5;
+// //         // byte r = 2;
+//         // char c = 'c';
+//         // byte bt = (byte) (q + r + c);
+//         // System.out.println(bt);
+     
+
+        
+//     }
+   
+// }
+//////////////////////////////// assignment ////////////////
+/*import java.util.*;
+ 
+public class JAVABASICS {
+    public static  void main (String args[]) {
+        int A = 15;
+        int B = 14;
+        int C = 13;
+        int avg = (A + B +C)/3;
+        System.out.println(avg);
+    // 2 
+     Scanner sc = new Scanner(System.in);
+     int side = sc.nextInt();
+     int area = side * side ;
+     System.out.println(" side of square is  = " + area );
+
+
+
+    }
+} */
+///////////////////////////////////////////////////////////////////////////
+    // import java.util.*;
+    // public class JAVABASICS {
+    //     public static void main (String args[]) {
+    //         Scanner sc = new Scanner(System.in);
+    //           float pencil = sc.nextFloat (); 
+    //           float pen = sc.nextFloat (); 
+    //           float eraser = sc.nextFloat (); 
+    //           float total = pencil + pen + eraser ;
+    //           System.out.println("bill is " + total);
+
+    //           float newtotal = total +(.18f * total);
+    //           System.out.println (" BILL WITH 18% GST" + newtotal); 15
+
+
+
+          
+    ////// thankyou////////////
