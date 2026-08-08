5553fbe442a73365f7d6c15b3617e12cfe52ea57
Sushant-arch-star <Sushantji045@gmail.com>
2026-08-08 14:09:36 +0530

    Added new java practise

--- loops.java
+++ loops.java
@@ -210,3 +210,347 @@
 //     System.out.println(rev);
 //   }
 // }
+///////////////////////////////////////////// do while loops //////////////////////////
+///////////////////////////////// for loops ////////////////////////////////////////////////////////////////////////////////////        
+// //      }
+
+// import java.util.*;
+// public class loops {
+//     public static void main(String args[])  {
+//         for(int i=1; i<=4; i++) {
+//             System.out.println(i + "puri duniya ki maa ki chut");
+
+//       
+
+//         }
+
+        
+//         }
+//      }
+ 
+
+// //   //print reverse of a number////////////////// 
+//   // n =10899
+
+  // import java.util.*;
+
+
+  // public class loops {
+  //   public static void main(String args[]) {
+  //       int n =10899;
+  //       while(n>0) {
+  //           int lastDigit = n % 10;
+  //           System.out.print(lastDigit);
+  //           n = n / 10;
+
+
+  //       }
+  //       System.out.println();
+
+        
+  //   }
+  // }
+
+///////////////////////////////////////////////////////////////// revese the given number //////////////////////////////////////////
+
+
+// import java.util.*;
+// public class loops {
+//     public static void main(String args[]) {
+//         int n = 10899;
+//         int rev = 0;
+         
+//         while (n > 0) {
+//             int lastDigit = n%10;
+//             rev = (rev*10) + lastDigit;
+//             n = n/10;
+
+
+//         }
+//         System.out.println(rev);
+
+      
+//     }
+
+// }
+
+//  import java.util.*;
+//  public class loops {
+//   public static void main(String args[]) {
+//     Scanner sc = new Scanner(System.in);
+//     int num = sc.nextInt();
+//     int rev = 0;
+//     while (num > 0) {
+//       int lastdigit = num%10;
+//        rev = (rev*10)+ lastdigit;
+//        num = num/10;
+//     }
+//     System.out.println(rev);
+//   }
+//  }
+
+////////////////////////////  do whie ////////////////////////////////////
+
+// import java.util.*;
+//  public class loops {
+//     public static void main(String args []) {
+//         int counter =1 ;
+//         do {
+//             System.out.print("LOOP SA BAHAR ");
+//             counter++;
+
+
+//         }
+//         while(counter <=12);
+
+//     }
+
+// // //////////////////////////////// BREAK STATEMENT /////////////
+
+
+// import java.util.*;
+// public class loops {
+//     public static void main(String args[]) {
+//         for (int i=1; i<=7; i++) {
+//              if(i == 3){
+//                 break;
+
+//              }
+//              System.out.print(i);
+           
+//         }
+//         System.out.println("i am bad for everyone");
+ 
+//     }
+  
+//////////////////////////////////////////// keep entering the number until user enters a multiple of 10///////////////////////////////////////////////////
+
+// import java.util.*;
+// public class loops {
+//   public static void main(String args[]) {
+
+//     Scanner sc = new Scanner(System.in);
+//      do {
+//       System.out.print("apna number likho :=");
+
+//       int n = sc.nextInt();
+//       if (n%10==0) {
+//         break;
+//       }
+//       System.out.println(n);
+
+//      } while(true);
+
+// }
+
+//}
+
+
+/////////////////////////// CONTINUE STATEMENT ////////////////////////////
+
+// import java.util.*;
+// public class loops {
+//   public static void main(String args []) {
+//     for (int i=1; i<=5; i++) {
+//             if (i==3) {
+//                continue;
+// }   System.out.println(i);
+//     }
+//   }
+// }
+////////////////////////////// or same qieston whith do whie method /////////////////////////////////
+// /// 
+// import java.util.*;
+// public class loops { 
+//   public static void main(String args[]) {
+//     int  i = 1;
+//     do {
+      
+//       if (i == 3) {
+//                 i++;
+
+//         continue;
+
+//       }
+//             System.out.println(i);
+//             i++;
+//     }
+//    while(i <= 19);
+//   }
+// }
+// /////////////////////////////////////////////// dusplay all no expext multiple of 10;/////////////////////////////////////////
+
+
+
+// import java.util.*;
+// public class loops {
+//   public static void main(String args[]) {
+//     Scanner sc = new Scanner(System.in);
+//     do {
+//       System.out.print("enter your sankhya := ");
+//       int n = sc.nextInt();
+
+//       if (n%10==0) {
+//         continue;
+
+//      }
+//       System.out.println("number was :" + n);
+
+//     }while(true);
+
+//     }
+// }
+//////////////////////////////////////// NUMBER IS PRIME OR NOT /////////////////
+// import java.util.*;
+// public class loops {
+//   public static void main (String args[]) {
+//     Scanner sc = new Scanner(System.in);
+//     int n = sc.nextInt();
+//      if (n==2 ) {
+//       System.out.println("n is prime ");
+
+//      }
+//     boolean isPrime = true;
+//     for (int i=2; i<= n-1 ; i++) {// i< Math.sqrt(n) //
+
+//       if(n%i== 0 ) {  // n is a multiple of i (i not equal to 1 or n )
+//         isPrime = false; 
+
+//       }
+//     } 
+//      if(isPrime == true) {
+//       System.out.println("n is prime");
+
+//      } else { 
+//       System.out.println("n is not a prime ");
+
+//      }  
+//   }
+// }
+   ////////////////////////////////// this code for from till the digit prime or not ////////////////////
+   
+// import java.util.*;
+// public class loops {
+//   public static void main(String args[]) {
+//     Scanner sc  = new Scanner(System.in);
+//     int num = sc.nextInt();
+//     for ( int i =1; i<=num; i++) {
+//       if(num % i==0) {
+//         System.out.println("prime = " + i);
+//       }
+//       else {
+//         System.out.println(  "not prime = " + i);
+//       }
+//     }
+    
+//   }
+// }
+// ////////////////////////////////////////////// no is prime or not  //////////////////////////////////////////////////////
+// import java.util.*; 
+// public class loops {
+//   public static void main(String args[]) {
+//     Scanner sc = new Scanner(System.in);
+//     int n =sc.nextInt();
+//     if (n<=1) {
+//       System.out.print("n is not a  prime ");
+//       return;
+//     }
+//    boolean isPrime = true;
+//    for(int i =2; i<=n-1; i++) {
+//       if(n%i==0) {
+//         isPrime = false;
+
+//       }
+    
+//    }
+// if (isPrime == true)
+//     {
+//             System.out.println("n is prime");
+
+//     }
+//       else 
+//       {
+//               System.out.println("n is not a prime");
+
+//       }
+//   }
+// }
+///////////////////////////////////////////////////// assignmennt //////////////////////////////////
+
+// Q1 ans is 2 times 
+
+// @Q2  
+
+
+
+/                                     // // next answer is /////////////////////////////////////////////////////////
+// import java.util.*;
+// public class loops {
+//   public static void main (String args[]) {
+//     Scanner sc = new Scanner(System.in);
+//          int number;
+//         int choice;
+//       int evenSum = 0;
+//     int oddSum = 0;
+
+//     do {
+//       System.out.print("Enter the number ");
+//       number = sc.nextInt();
+
+//       if (number % 2 == 0) {
+//         evenSum += number;
+//       } else {
+//         oddSum += number;
+
+//       }
+//       System.out.print(" do you want to continue ? press 1 for yes or 0 for no ");
+//       choice = sc.nextInt();
+
+//       } while (choice ==1);
+//       System.out.println("sum of even number is" + evenSum);
+//       System.out.println("sum of odd number is" + oddSum);
+
+
+
+      
+//     }
+//    }
+
+// import java.util.*;
+// public class loops {
+//   public static void main(String args[]) {
+//     Scanner sc = new Scanner(System.in);
+//     int num;
+//     int fact = 1;
+//     System.out.print("ENTER YOUR NUMBER ;=");
+//     num = sc.nextInt();
+//     for (int i = 1; i <= num; i++) {
+//       fact = fact * i;
+
+//     }
+//     System.out.println("factorial :1 " + fact);
+
+// }
+// }
+
+// import java.util.*;
+// class MultiplicationTable {
+// public static void printMultiplicationTable(int numbers) {
+//   Scanner sc = new Scanner(System.in);
+//   System.out.print("enter number :--->");
+//   int n = sc.nextInt();
+//   for ( int i=1; i<=10; i++) {
+//     System.out.println(n + " * " + i + "=" + n*i);
+
+//   }
+
+// }
+// public static void main (String s[]) {
+//   printMultiplicationTable(5);
+// }
+
+
+// }
+
+
+//////////////////////////////////////// thankyou////////////////////////
