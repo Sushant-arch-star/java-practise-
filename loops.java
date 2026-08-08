632e222c17ae4906a0429d56293af256d4cbafd9
Sushant-arch-star <Sushantji045@gmail.com>
2026-08-08 11:14:05 +0530

    Added loops practise

---
+++ loops.java
@@ -0,0 +1,212 @@
+ ///////////////////////  loops  lecture 5 ///////////////////////
+ // while loops 
+
+// import java.util.*;
+// public class loops {
+//     public static void main(String args[])  {
+//         int counter = 0;
+//         while(counter < 100) {
+//             System.out.println("sushant tiwari");
+//             counter++;
+
+//         }
+//         System.out.println("sushant tiwari 100 times done");
+
+
+//     }
+// }
+
+// // // no from 1 to 10 using while loop
+
+// import java.util.*;
+// public class loops {
+//      public static void main(String args[]) {
+//         int c = 1;
+//         while(c<11) {
+//             System.out.print(  c+"fuck");
+//             c++;
+//         }
+//         System.out.println("fuck u ");
+//       }
+
+
+//     }
+
+// //         }
+// //     //  note yaad rakhna agar upar wala na 100 hi printhua kyu ki humna counter bahar print nahi karya is liya 101 times print nahi hua 
+//     //  wahi nicha wala main hame 1 sa 10 tak janna hai to syso(counter); karenga to 11 print hoga kyu ki lopp ka nbahar bhi code run ho ra hai okk
+
+// // //no from 1 to 10 using while loop
+
+//  import java.util.*;
+//  public class loops {
+//      public static void main(String args[]) {
+//         int c = 1;
+//         while(c<=10) {
+//             System.out.print(c + "fuked ");
+//             c++;
+
+//         }            System.out.println();
+
+        
+
+
+// }
+
+//  }
+
+// print no from 1 to n (while loop )
+//  import java.util.*;
+//  public class loops {
+//       public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         int range = sc.nextInt();
+//         int counter = 1;
+//         while(counter<=range) {
+//             System.out.print(counter +" ");
+//            counter++;
+
+
+//         }
+//           System.out.println();
+
+
+//       }
+//     } 
+
+ 
+// import java.util.*;
+// public class loops {
+//   public static void main(String args[]) {
+//     Scanner sc = new Scanner(System.in);
+//     int num = sc.nextInt();
+//     int c = 1;
+//     while(c<=num) {
+//       System.out.print(num + " ");
+//       c++;
+//     }
+//     System.out.println();
+//   }
+// }
+
+ 
+// // //print sum of first n natural numner (while loop)
+// //  import java.util.*;
+// //  public class loops {
+// //      public static void main(String args[]) {
+// //                Scanner sc = new Scanner(System.in);
+// //                int n =sc.nextInt();
+//                int sum =0;
+
+
+//                int i=1;
+//                while(i<=n) {
+//                  sum+= i;   
+//                i++;
+
+//                }
+              
+
+     
+//            System.out.println("sum is " + sum
+        
+//            );
+///////////////////////////////// for loops ////////////////////////////////////////////////////////////////////////////////////        
+// //      }
+
+// import java.util.*;
+// public class loops {
+//     public static void main(String args[])  {
+//         for(int i=1; i<=4; i++) {
+//             System.out.println(i + "puri duniya ki maa ki chut");
+
+//       
+
+//         }
+
+        
+//         }
+//      }
+ 
+
+// //   //print reverse of a number////////////////// 
+//   // n =10899
+
+  // import java.util.*;
+
+
+  // public class loops {
+  //   public static void main(String args[]) {
+  //       int n =10899;
+  //       while(n>0) {
+  //           int lastDigit = n % 10;
+  //           System.out.print(lastDigit);
+  //           n = n / 10;
+
+
+  //       }
+  //       System.out.println();
+
+        
+  //   }
+  // }
+
+// //   //print reverse of a number////////////////// 
+//   // n =10899
+
+  // import java.util.*;
+
+
+  // public class loops {
+  //   public static void main(String args[]) {
+  //       int n =10899;
+  //       while(n>0) {
+  //           int lastDigit = n % 10;
+  //           System.out.print(lastDigit);
+  //           n = n / 10;
+
+
+  //       }
+  //       System.out.println();
+
+        
+  //   }
+  // }
+
+///////////////////////////////////////////////////////////////// revese the given number //////////////////////////////////////////
+
+
+// import java.util.*;
+// public class loops {
+//     public static void main(String args[]) {
+//         int n = 10899;
+//         int rev = 0;
+         
+//         while (n > 0) {
+//             int lastDigit = n%10;
+//             rev = (rev*10) + lastDigit;
+//             n = n/10;
+
+
+//         }
+//         System.out.println(rev);
+
+      
+//     }
+
+// }
+
+//  import java.util.*;
+//  public class loops {
+//   public static void main(String args[]) {
+//     Scanner sc = new Scanner(System.in);
+//     int num = sc.nextInt();
+//     int rev = 0;
+//     while (num > 0) {
+//       int lastdigit = num%10;
+//        rev = (rev*10)+ lastdigit;
+//        num = num/10;
+//     }
+//     System.out.println(rev);
+//   }
+// }
