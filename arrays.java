cb227e246f562d7f95a4f6b67e7480923f583666
Sushant-arch-star <Sushantji045@gmail.com>
2026-08-10 15:17:47 +0530

    Adding arrays practise

--- arrays.java
+++ arrays.java
@@ -233,37 +233,34 @@ binary will perform better ia any case if there is a log then its is bstter comp
 
 
 
-// // import java.util.*; 
-// // public class ARRAYS {
-// //     public static void printsubarrays(int numbers[]) {
-// //         int  ta = 0;
-// //         for (int i=0; i<numbers.length; i++)  {
-// //             int start = i;
+// import java.util.*; 
+// public class arrays {
+//     public static void printsubarrays(int numbers[]) {
+//         int  ta = 0;
+//         for (int i=0; i<numbers.length; i++)  {
+//             int start = i;
 
-// //             for ( int j=i; j<numbers.length; j++) {
-// //                 int end = j; 
-// //                  for (int k = start; k<=end; k++) {
-// //                     System.out.print(numbers[k] + " ");
-// //                  }
-// //                  ta++;
-// //                  System.out.println();
+//             for ( int j=i; j<numbers.length; j++) {
+//                 int end = j; 
+//                  for (int k = start; k<=end; k++) {
+//                     System.out.print(numbers[k] + " ");
+//                  }
+//                  ta++;
+//                  System.out.println();
 
-// //             }
-// //                  System.out.println();
+//             }
+//                  System.out.println();
+//         }
 
+//          System.out.println(" total  subarrays = " + ta);
 
-// //         }
-
-// //                          System.out.println(" total  subarrays = " + ta);
-
-
-// //     }
-
-    
+//     }
 //     public static void main(String args[]) {
-//         int numbers[]= { 2,4,6,8,10};
+//         int numbers[]= {2,4,6,8,10};
 //        printsubarrays(numbers);
 
 
 //     }
+// }
         // }
+        
