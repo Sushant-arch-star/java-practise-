caee381d4e1c3a1f66bbfa740171e50c15340784
Sushant-arch-star <Sushantji045@gmail.com>
2026-08-21 13:56:43 +0530

    Adding conditional practise

--- conditional.java
+++ conditional.java
@@ -82,7 +82,7 @@
 //         int number = sc.nextInt();
 //         if(number%2==0) 
 //             System.out.println("even");
-//        else
+       
 //         System.out.println("odd");
 
 //     }
